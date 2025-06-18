a4b87a39214e78b62dacbda8bd5a0449ef55ee6c
Arsen Tabaku <arsentabaku2@gmail.com>
2025-06-18 16:24:25 +0200

    Fixed cors problem

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
 import random
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 flashcards = []
 
